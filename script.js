df66b568dcaa39308a202f75b8c8f7b522312286
Virab-Maestro <khachatryanvirab7@gmail.com>
2024-02-26 14:10:42 +0300

    update players section - mobile 1.0

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 //Desktop slider steps
-
-new Swiper(".steps__slider", {
-    slidesPerView: 3,
+const steps = new Swiper(".steps__slider", {
+    slidesPerView: 1,
     spaceBetween: 30,
-    watchOverflow: true,
     pagination: {
       el: ".swiper-pagination",
-      type: 'custom',
-      renderCustom: function (swiper, current, total) {
-          console.log(swiper);
-          return 2 + current + '/' + (swiper.slides.length); 
-      }
+    //   type: 'custom',
     },
     navigation:{
         nextEl: ".players-button-next",
@@ -19,17 +13,49 @@ new Swiper(".steps__slider", {
 });
 
 //Desktop slider players
-
-new Swiper(".swiper", {
+let players = new Swiper(".swiper__desktop", {
     slidesPerView: 3,
     spaceBetween: 30,
-    watchOverflow: true,
+
     pagination: {
       el: ".swiper-pagination",
       type: 'custom',
       renderCustom: function (swiper, current, total) {
-          console.log(swiper);
-          return 2 + current + '/' + (swiper.slides.length); 
+        return 2 + current + '<span>' + '/' + (swiper.slides.length) + '</span>'; 
+      }
+    },
+    navigation:{
+        nextEl: ".players-button-next",
+        prevEl: ".players-button-prev"
+    }
+});
+//laptop slider players
+players = new Swiper(".swiper__laptop", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+
+    pagination: {
+      el: ".swiper-pagination",
+      type: 'custom',
+      renderCustom: function (swiper, current, total) {
+        return 1 + current + '<span>' + '/' + (swiper.slides.length) + '</span>'; 
+      }
+    },
+    navigation:{
+        nextEl: ".players-button-next",
+        prevEl: ".players-button-prev"
+    }
+});
+//mobile slider players
+players = new Swiper(".swiper__mobile", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+
+    pagination: {
+      el: ".swiper-pagination",
+      type: 'custom',
+      renderCustom: function (swiper, current, total) {
+          return current + '<span>' + '/' + (swiper.slides.length) + '</span>'; 
       }
     },
     navigation:{
@@ -53,3 +79,46 @@ const observer = new IntersectionObserver((entries)=>{
 animatedElements.forEach(el=>{
     observer.observe(el);
 })
+
+// slidesPerView according to the screen size (for test)
+// window.addEventListener('resize', function() {
+//     if (screen.width > 1024) {
+//         slidesPerView = 3;
+//     }else if(screen.width <= 1024){
+//         slidesPerView = 2;
+//     }
+//     else if(screen.width <= 768){
+//         slidesPerView = 1;
+//     }
+//     // console.log('resized')
+//     players = new Swiper(".swiper", {
+//         slidesPerView: slidesPerView,
+//         spaceBetween: 30,
+//         watchOverflow: true,
+//         pagination: {
+//           el: ".swiper-pagination",
+//           type: 'custom',
+//           renderCustom: function (swiper, current, total) {
+//               console.log(swiper);
+//               return 2 + current + '/' + (swiper.slides.length); 
+//           }
+//         },
+//         navigation:{
+//             nextEl: ".players-button-next",
+//             prevEl: ".players-button-prev"
+//         }
+//     });
+// })
+
+// slidesPerView according to the screen size
+// if (screen.width > 1024) {
+//     slidesPerView = 3;
+//     changePlayersSlidesCount(slidesPerView);
+// }else if(screen.width <= 1024 && screen.width > 640){
+//     slidesPerView = 2;
+//     changePlayersSlidesCount(slidesPerView);
+// }
+// else if(screen.width <= 640){
+//     slidesPerView = 1;
+//     changePlayersSlidesCount(slidesPerView);
+// }
