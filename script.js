afea673116829b5d02ccb4c5d0c6218294bcb2ce
Virab-Maestro <khachatryanvirab7@gmail.com>
2025-08-10 10:57:29 +0300

    swiper to vaniliaJS: players slider

--- script.js
+++ script.js
@@ -64,10 +64,85 @@ players = new Swiper(".swiper__mobile", {
     }
 });
 
+
+//Players slider
+const sliderInner = document.querySelector(".players__slider-inner"),
+    slides = document.querySelectorAll(".players__slide"),
+    slidedVal = document.querySelector(".players__slider-pagination .slider__slided"),
+    totalVal = document.querySelector(".players__slider-pagination .slider__total"),
+    slidesToShow = Number(window.getComputedStyle(document.body).getPropertyValue("--slides-to-show")),
+    slideWidth = Number(window.getComputedStyle(slides[0]).width.match(/(-?\d+)px$/)[1]),
+    slideGap = Number(window.getComputedStyle(sliderInner).gap.match(/(-?\d+)px$/)[1]),
+    offsetToLastSlide = -(slides.length - slidesToShow) * (slideWidth + slideGap);
+
+//initializing slider position(nums)
+slidedVal.textContent = slides.length > slidesToShow ? slidesToShow : slides.length;
+totalVal.textContent = slides.length;
+function setPos(direction) {//0 - prev; 1 - next
+    if(direction == 0 && slidedVal.textContent <= slidesToShow) {
+        slidedVal.textContent = totalVal.textContent;
+        return;
+    }else if(direction == 1 && slidedVal.textContent == slides.length) {
+        slidedVal.textContent = slides.length > slidesToShow ? slidesToShow : slides.length;
+        return;
+    }
+
+    if(direction == 0) {
+        console.log('ch0')
+        slidedVal.textContent = +slidedVal.textContent - 1;
+    }else {
+        console.log('ch1')
+        slidedVal.textContent = +slidedVal.textContent + 1;
+    }
+}
+
+//next, prev
+sliderInner.style.transform = "translateX(0px)";
+function prevSlide() {
+    //restart sliding interval
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, 4000)
+
+    //update slided number preview
+    setPos(0);
+
+    //slide
+    const currentOffset =  sliderInner.style.transform.match(/\w+\((-?\d+)px\)$/)[1];
+    if(currentOffset == 0 && slides.length > slidesToShow) {
+        sliderInner.style.transform = `translateX(${offsetToLastSlide}px)`;
+        return;
+    }
+    if(slides.length > slidesToShow) {
+        sliderInner.style.transform = `translateX(${+currentOffset + (slideWidth + slideGap)}px)`;
+    }
+}
+
+function nextSlide() {
+    //restart sliding interval
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, 4000)
+
+    //update slided number preview
+    setPos(1);
+
+    //slide
+    const currentOffset =  sliderInner.style.transform.match(/\w+\((-?\d+)px\)$/)[1];
+    if(currentOffset == offsetToLastSlide) {
+        sliderInner.style.transform = `translateX(0px)`;
+        return;
+    }
+    if(slides.length > slidesToShow) {
+        sliderInner.style.transform = `translateX(${+currentOffset - (slideWidth + slideGap)}px)`;
+    }
+
+}
+
+//slide every 4s
+let slideInterval = setInterval(nextSlide, 4000);
+
+
 //Animations
-
 const animatedElements = document.querySelectorAll('.animation-hide');
-
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach(entry=>{
         if(entry.isIntersecting){
@@ -75,50 +150,6 @@ const observer = new IntersectionObserver((entries)=>{
         }
     })
 })
-
 animatedElements.forEach(el=>{
     observer.observe(el);
 })
-
-// slidesPerView according to the screen size (for test)
-// window.addEventListener('resize', function() {
-//     if (screen.width > 1024) {
-//         slidesPerView = 3;
-//     }else if(screen.width <= 1024){
-//         slidesPerView = 2;
-//     }
-//     else if(screen.width <= 768){
-//         slidesPerView = 1;
-//     }
-//     // console.log('resized')
-//     players = new Swiper(".swiper", {
-//         slidesPerView: slidesPerView,
-//         spaceBetween: 30,
-//         watchOverflow: true,
-//         pagination: {
-//           el: ".swiper-pagination",
-//           type: 'custom',
-//           renderCustom: function (swiper, current, total) {
-//               console.log(swiper);
-//               return 2 + current + '/' + (swiper.slides.length); 
-//           }
-//         },
-//         navigation:{
-//             nextEl: ".players-button-next",
-//             prevEl: ".players-button-prev"
-//         }
-//     });
-// })
-
-// slidesPerView according to the screen size
-// if (screen.width > 1024) {
-//     slidesPerView = 3;
-//     changePlayersSlidesCount(slidesPerView);
-// }else if(screen.width <= 1024 && screen.width > 640){
-//     slidesPerView = 2;
-//     changePlayersSlidesCount(slidesPerView);
-// }
-// else if(screen.width <= 640){
-//     slidesPerView = 1;
-//     changePlayersSlidesCount(slidesPerView);
-// }
